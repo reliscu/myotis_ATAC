aeaa2f24cdf0ddb56f2c37b813b25046a230432c
reliscu <rebeliscu@gmail.com>
2024-01-03 12:58:00 -0800

    finding intersections in paf files

--- analysis/reciprocal_best_hits/bedtools_intersect.sh
+++ analysis/reciprocal_best_hits/bedtools_intersect.sh
@@ -5,13 +5,20 @@ species=($(awk -F"," 'NR>1 {print $2}' ../../../../data/myotis_meta.csv))
 bedtools2=/Users/rebecca/programs/bedtools2/bin/bedtools
 
 for spec1 in ${species[@]}; do
+
     for spec2 in ${species[@]}; do
+
         if [ $spec1 != $spec2 ]; then
+
             echo $spec1 vs. $spec2
-            $bedtools2 intersect -wo \
-                -a ${spec1}_vs_${spec2}_${spec1}_positions_target.bed \
-                -b ${spec1}_vs_${spec2}_${spec1}_positions_query.bed \
-                > ${spec1}_vs_${spec2}_${spec1}_positions_intersect.bed
+
+            $bedtools2 intersect -wb \
+                -a ${spec1}_vs_${spec2}_positions_target.bed \
+                -b ${spec1}_vs_${spec2}_positions_query.bed \
+                > ${spec1}_vs_${spec2}_positions_intersect.bed
+
         fi
+
     done
+
 done
