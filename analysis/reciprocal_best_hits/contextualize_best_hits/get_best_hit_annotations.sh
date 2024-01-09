77580234815905812d091becacae40b549129ccc
reliscu <rebeliscu@gmail.com>
2024-01-09 13:42:44 -0800

    best hit annotations

--- analysis/reciprocal_best_hits/contextualize_best_hits/get_best_hit_annotations.sh
+++ analysis/reciprocal_best_hits/contextualize_best_hits/get_best_hit_annotations.sh
@@ -1,24 +1,51 @@
-cd /Users/rebecca/sudmant/analyses/myotis/analysis/reciprocal_best_hits/results
+cd /Users/rebecca/sudmant/analyses/myotis/analysis/reciprocal_best_hits/get_best_hits/results
+
+## Get genes and TEs associated with reciprocal best hits:
+
+bedtools2=/Users/rebecca/programs/bedtools2/bin/bedtools
 
 for ea in *csv; do
 
   spec1=$(awk -F"," 'NR==2{print $1}' $ea | sed "s/1.*$//g")
   spec2=$(awk -F"," 'NR==2{print $6}' $ea | sed "s/1.*$//g")
-  awk -F"," 'NR>1{print $1, $3, $4}' $ea > ${spec1}_${spec1}_vs_${spec2}.bed
-  awk -F"," 'NR>1{print $6, $8, $9}' $ea > ${spec2}_${spec1}_vs_${spec2}.bed
 
-done
+  ## Get intersecting sequences into BED format for each species separately:
+  awk -v pattern="${spec1}1.0." -F"," 'NR>1{gsub(pattern, "", $1); print $1, $3, $4}' OFS='\t' $ea > spec1.bed
+  awk -v pattern="${spec2}1.0." -F"," 'NR>1{gsub(pattern, "", $6); print $6, $8, $9}' OFS='\t' $ea > spec2.bed
 
-## Use intersect BEDs with annotation files:
+  ## Cross ref. intersecting sequences with genes:
+  
+  gff1="/Users/rebecca/sudmant/analyses/myotis/data/genomes/bat_genomes/gff_final_curated/${spec1}1_finalAnnotation.gff3"
+  gff2="/Users/rebecca/sudmant/analyses/myotis/data/genomes/bat_genomes/gff_final_curated/${spec2}1_finalAnnotation.gff3"
 
-cd /Users/rebecca/sudmant/analyses/myotis/analysis/reciprocal_best_hits/results
-bedtools2=/Users/rebecca/programs/bedtools2/bin/bedtools
+  if [ $(head $gff1 | grep -c SCAF) -gt 0 ]; then echo sed -i  "s/SCAF/SUPER/g" $gff1; fi
+  if [ $(head $gff2 | grep -c SCAF) -gt 0 ]; then echo sed -i "s/SCAF/SUPER/g" $gff2; fi
 
-for ea in *bed; do
+  $bedtools2 intersect -wb -a $gff1 -b spec1.bed > 
+    ../../contextualize_best_hits/results/${spec1}_${spec1}_vs_${spec2}_best_hits_gene_annotations.csv
+  $bedtools2 intersect -wb -a $gff2 -b spec2.bed > 
+    ../../contextualize_best_hits/results/${spec2}_${spec1}_vs_${spec2}_best_hits_gene_annotations.csv 
+  
+  ## Cross ref. intersecting sequences with TEs:
 
-  spec1=$(echo $ea | sed "s/_.*$//g")
-  spec2=$(echo $ea | cut -c 9- | sed "s/_vs_.*$//g")
-  gff="/Users/rebecca/sudmant/data/myotis/genomes/bat_genomes/gff_final_curated/${spec1}1_finalAnnotation.gff3"
-  $bedtools2 intersect -wb -a $gff -b $ea > ${spec1}_${spec1}_vs_${spec2}_gene_annotations.csv
+  gff1="/Users/rebecca/sudmant/analyses/myotis/data/genomes/bat_genomes/repeatMasker/${spec1}1_sorted.fas.out"
+  gff2="/Users/rebecca/sudmant/analyses/myotis/data/genomes/bat_genomes/repeatMasker/${spec2}1_sorted.fas.out"
+
+  ## Get TE annotations into BED format:
+  if [ -f ../../contextualize_best_hits/resources/${spec1}_TEs.bed ]; then
+    awk 'NR>3{gsub("SCAF", "SUPER", $5); print $5, $6, $7, $1, $2, $9, $11}' OFS='\t' $gff1 >
+      ../../contextualize_best_hits/resources/${spec1}_TEs.bed
+  fi
+  if [ -f ../../contextualize_best_hits/resources/${spec1}_TEs.bed ]; then
+    awk 'NR>3{gsub("SCAF", "SUPER", $5); print $5, $6, $7, $1, $2, $9, $11}' OFS='\t' $gff2 > 
+      ../../contextualize_best_hits/resources/${spec2}_TEs.bed
+  fi
+
+  $bedtools2 intersect -wb -a ../../contextualize_best_hits/resources/${spec1}_TEs.bed -b spec1.bed > 
+    ../../contextualize_best_hits/results/${spec1}_${spec1}_vs_${spec2}_best_hits_TE_annotations.csv
+  $bedtools2 intersect -wb -a ../../contextualize_best_hits/resources/${spec2}_TEs.bed $gff2 -b spec2.bed > 
+    ../../contextualize_best_hits/results/${spec2}_${spec1}_vs_${spec2}_best_hits_TE_annotations.csv
+
+  rm spec1.bed spec2.bed
 
 done
--- analysis/reciprocal_best_hits/get_best_hits/blast_hits.sh
+++ analysis/reciprocal_best_hits/get_best_hits/blast_hits.sh
@@ -1,7 +1,7 @@
 ## Make reference DB
 
 for ea in *fa; do
-   makeblastdb -in /Users/rebecca/sudmant/data/myotis/genomes/bat_genomes/mMyoCai1.cleaned.hapheader.fa \
+   makeblastdb -in /Users/rebecca/sudmant/analyses/myotis/data/genomes/bat_genomes/mMyoCai1.cleaned.hapheader.fa \
     -parse_seqids \
     -dbtype nucl \
     -out mMyoCai
@@ -9,21 +9,40 @@ done
 
 ## Make query sequence (BED -> FASTA):
 
-dat <- fread("results/mMyoAui_vs_mMyoCai_reciprocal_best_hits.csv", data.table = FALSE)
+dat <- fread("/Users/rebecca/sudmant/analyses/myotis/analysis/reciprocal_best_hits/get_best_hits/results/mMyoAui_vs_mMyoCai_reciprocal_best_hits.csv", data.table = FALSE)
 dat <- dat[1, c(1, 3:4)]
-write.table(dat, file = "mMyoAui_vs_mMyoCai_hits.bed", sep = "\t", row.names = FALSE, quote = FALSE, col.names = FALSE)
+write.table(dat, file = "mMyoAui_mMyoAui_vs_mMyoCai_hits.bed", sep = "\t", row.names = FALSE, quote = FALSE, col.names = FALSE)
 
 bedtools2=/Users/rebecca/programs/bedtools2/bin/bedtools
 
-cd /Users/rebecca/sudmant/analyses/myotis/analysis/reciprocal_best_hits/
+cd /Users/rebecca/sudmant/analyses/myotis/analysis/reciprocal_best_hits/get_best_hits
 
 $bedtools2 getfasta \
   -fi /Users/rebecca/sudmant/data/myotis/genomes/bat_genomes/mMyoAui1.cleaned.hapheader.fa \
-  -bed /Users/rebecca/sudmant/analyses/myotis/analysis/reciprocal_best_hits/mMyoAui_vs_mMyoCai_hits.bed > mMyoAui_seq.fasta
+  -bed /Users/rebecca/sudmant/analyses/myotis/analysis/reciprocal_best_hits/get_best_hits/mMyoAui_mMyoAui_vs_mMyoCai_hits.bed > mMyoAui_seq.fasta
+
+## Blast query against reference:
 
 cd /Users/rebecca/blastdb
 
-blastn -query /Users/rebecca/sudmant/analyses/myotis/analysis/reciprocal_best_hits/mMyoAui_seq.fasta \
+blastn \
+  -query /Users/rebecca/sudmant/analyses/myotis/analysis/reciprocal_best_hits/get_best_hits/mMyoAui_seq.fasta \
   -db mMyoCai \
   -out mMyoAui_vs_mMyoCai.txt \
   -num_threads 4
+
+####################
+
+blastn \
+  -query /Users/rebecca/sudmant/data/myotis/genomes/bat_genomes/mMyoAui1.cleaned.hapheader.fa \
+  -subject /Users/rebecca/sudmant/data/myotis/genomes/bat_genomes/mMyoCai1.cleaned.hapheader.fa \
+  -query_loc 231190000-234005000 \
+  -outfmt 6 \
+  -out mMyoAui_query_vs_mMyoCai_subject.txt 
+
+blastn \
+  -query /Users/rebecca/sudmant/data/myotis/genomes/bat_genomes/mMyoAui1.cleaned.hapheader.fa \
+  -subject /Users/rebecca/sudmant/data/myotis/genomes/bat_genomes/mMyoCai1.cleaned.hapheader.fa \
+  -subject_loc 40112-2853759 \
+  -outfmt 6 \
+  -out mMyoAui_query_vs_mMyoCai_subject.txt 
