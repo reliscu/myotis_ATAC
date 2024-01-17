c52b1cef9f411b4e85788d8f985f746fcbf4bf6d
reliscu <rebeliscu@gmail.com>
2024-01-17 14:29:05 -0800

    summary of peaks in recip regions

--- analysis/reciprocal_best_hits/contextualize_best_hits/04_intersect_best_hits_with_peaks.sh
+++ analysis/reciprocal_best_hits/contextualize_best_hits/04_intersect_best_hits_with_peaks.sh
@@ -8,17 +8,16 @@ bedtools2=/Users/rebecca/programs/bedtools2/bin/bedtools
 
 for (( i=0; i<$len; i++ )); do
   echo ${field_names[$i]}
-  ## Get consensus peaks for working species:
+  # Get consensus peaks for working species:
   peaks="/Users/rebecca/sudmant/analyses/myotis/data/ATAC-seq/genes/${field_names[$i]}/Consensus_Peaks_Annotations_Accross_All_Samples.csv"
-  ## Get into BED format:
-  echo $hello
-  awk -F"\t" 'NR>1{gsub("SCAF", "SUPER", $2); print $2, $3, $4, $1, $5, $6, $7, $8, $9, $10, $11, $12, $(NF-2), $(NF-1), $NF}' OFS='\t' $peaks > peaks.bed
+  # Get into BED format:
+  awk -F"\t" 'NR>1{gsub("SCAF", "SUPER", $2); print $2, $3, $4, $1}' OFS='\t' $peaks > peaks.bed
   for hits in ${abbr_names[$i]}*; do
-    ## Get reciprocal sequences into BED format:
+    # Get reciprocal sequences into BED format:
     awk -v pattern="${abbr_names[$i]}1.0." -F"," 'NR>1{gsub(pattern, "", $1); print $1, $3, $4}' OFS='\t' $hits > hits.bed
-    ## Intersect peaks with reciprocal regions:
+    # Intersect peaks with reciprocal regions:
     $bedtools2 intersect -wb -a peaks.bed -b hits.bed > \
-      ../../contextualize_best_hits/resources/peaks/$(echo $hits | sed 's/.csv/_peaks.tsv/')
+      ../../contextualize_best_hits/resources/genes/peaks/$(echo $hits | sed 's/.csv/_peaks.tsv/')
     rm hits.bed
   done
  rm peaks.bed
---
+++ analysis/reciprocal_best_hits/get_best_hits/06_chrom_size.sh
@@ -0,0 +1,11 @@
+cd /Users/rebecca/sudmant/analyses/myotis/data
+
+## Make genome files with chromosome length per species:
+
+abbr_names=($(awk -F"," 'NR>1{print $2}' /Users/rebecca/sudmant/analyses/myotis/data/myotis_meta.csv))
+
+for ea in ${abbr_names[@]}; do
+  echo $ea
+  faidx /Users/rebecca/sudmant/analyses/myotis/data/genomes/bat_genomes/${ea}1.cleaned.hapheader.fa \
+    -i chromsizes | sed "s/SCAF/SUPER/" | sed "s/#0#/.0./" | sed "s/${ea}1.0.//g" > ${ea}_chromsizes
+done
