bbe693818af6faa10a7c06dbd1597875131f4d87
reliscu <rebeliscu@gmail.com>
2024-02-06 11:46:08 -0800

    fix RBH code

--- analysis/reciprocal_best_hits/01_get_best_hits/code/rbh_fns.R
+++ analysis/reciprocal_best_hits/01_get_best_hits/code/rbh_fns.R
@@ -58,7 +58,7 @@ spec_intersect_fn <- function(spec_subj_paf, spec_query_paf, spec.no) {
   temp_query <- temp_query[temp_query$ID %in% temp_subj$ID,]
   temp_subj <- temp_subj[match(temp_query$ID, temp_subj$ID),]
   spec_intersect <- cbind(temp_query$Name, temp_subj)
-  colnames(spec_intersect)[1:2] <- c("Query_ID", "Subject_ID")
+  colnames(spec_intersect)[1:2] <- c("Query_ID", "Subj_ID")
   colnames(spec_intersect) <- paste0("Spec", spec.no, "_", colnames(spec_intersect)) 
   
   return(spec_intersect)
